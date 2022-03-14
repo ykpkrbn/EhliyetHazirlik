3b5f7290513b74ed1e9755ac85e86fc0ee1963c8
Yakup Kurban <yakup-afyon-bjk@hotmail.com>
2022-03-14 18:19:36 +0300

    sonuç ekranında düzenlenme gerçekleştirildi

--- lib/ui/screens/sonuc_ekrani.dart
+++ lib/ui/screens/sonuc_ekrani.dart
@@ -14,6 +14,7 @@ class SonucEkrani extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: anaRenk,
       body: Column(
@@ -43,6 +44,70 @@ class SonucEkrani extends StatelessWidget {
             style: TextStyle(
                 color: yesilRenk, fontSize: 80, fontWeight: FontWeight.bold),
           ),
+          size < 390 ?
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              MaterialButton(
+                onPressed: () {
+                  Scorelar yeniSkorumuz =
+                  Scorelar(score: score, tarih: DateTime.now());
+                  DBProvider.dataBase.skorEkle(yeniSkorumuz);
+
+                  Navigator.pushNamedAndRemoveUntil(
+                      context, "/SkorlarEkrani", (context) => false);
+                },
+                color: ikinciRenk,
+                shape: const StadiumBorder(),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                        height: MediaQuery.of(context).size.height / 8,
+                        child: ResimlerYolu().basarilarim),
+                    Text(
+                      "Skorları \nGöster",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: beyazRenk,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10),
+              MaterialButton(
+                onPressed: () {
+                  Scorelar yeniSkorumuz =
+                  Scorelar(score: score, tarih: DateTime.now());
+                  DBProvider.dataBase.skorEkle(yeniSkorumuz);
+
+                  Navigator.pushNamedAndRemoveUntil(
+                      context, "/", (context) => false);
+                },
+                color: ikinciRenk,
+                shape: const StadiumBorder(),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                        height: MediaQuery.of(context).size.height / 8,
+                        child: ResimlerYolu().uygulamam),
+                    Text(
+                      "Ana Sayfaya \nGit",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: beyazRenk,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ):
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
